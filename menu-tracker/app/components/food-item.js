b308b572e0b1097265ad61b5f3a683f02129f9a5
Mark Cooper <mdcoopir@gmail.com>
2017-08-25 06:10:45 -0500

    Starting Course 3

--- menu-tracker/app/components/food-item.js
+++ menu-tracker/app/components/food-item.js
@@ -1,14 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isOpen: false,
+
   actions: {
     toggleAvailablity(food) {
       food.toggleProperty('isAvailable');
       food.save();
-    },
-    toggleOpen() {
-      this.toggleProperty('isOpen');
     },
     destroyItem(food) {
       food.destroyRecord();
